949f5b392a13b351af7a8b6cf9a7765ab2e1d046
Mohamed Mokashifi <132586649+Eng-Moka@users.noreply.github.com>
2024-02-13 11:59:12 +0300

    Domain_to_TextField

---
+++ Domain_to_TextField.py
@@ -0,0 +1,56 @@
+import arcpy
+import os
+
+
+def get_gdb_path_of_layer(layer_name):
+    aprx = arcpy.mp.ArcGISProject("current")
+    mp = aprx.activeMap
+    layer = [layer for layer in mp.listLayers() if layer.name == layer_name][0]
+    if layer:
+        return os.path.dirname(layer.dataSource)
+    else:
+        return ""
+
+
+def Domain_to_text(fc, feild_name, new_feild_name):
+
+    # Extract domain name from field Des
+    desc = arcpy.Describe(fc)
+    field = [x for x in desc.fields if x.name == feild_name][0]
+    field_domain = field.domain
+
+    GDB = get_gdb_path_of_layer(fc)
+    # Get the domain from fc GDB
+    domain = [x for x in arcpy.da.ListDomains(GDB) if x.name == field_domain][0]
+    if domain.domainType != "CodedValue":
+        arcpy.AddError(f"Domain {field_domain} is NOT coded value")
+        return
+    domain_dec = domain.codedValues
+    
+    arcpy.AddMessage(f"{domain_dec}")
+    arcpy.AddMessage(f"{domain.name}")
+    
+
+    arcpy.management.AddField(fc, new_feild_name, "TEXT", None, None, None, new_feild_name, "NULLABLE", "NON_REQUIRED", '')
+
+            
+    with arcpy.da.UpdateCursor(fc,[feild_name,new_feild_name]) as cursor:
+        for row in cursor:
+            for key,val in domain_dec.items():
+                if row[0] == key:
+                    row[1] = val
+            cursor.updateRow(row)
+
+
+# This is used to execute code if the file was run but not imported
+if __name__ == '__main__':
+
+    # Tool parameter accessed with GetParameter or GetParameterAsText
+    # gdb = arcpy.GetParameterAsText(0)
+    fc = arcpy.GetParameterAsText(0)
+    feild_name = arcpy.GetParameterAsText(1)
+    new_feild_name = arcpy.GetParameterAsText(2)
+    
+    Domain_to_text(fc,feild_name,new_feild_name)
+    
+    # Update derived parameter values using arcpy.SetParameter() or arcpy.SetParameterAsText()
